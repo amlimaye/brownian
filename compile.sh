ad823aa6faf9b07f24e911ba1ced42965336f638
Aditya Limaye <amlimaye@berkeley.edu>
2017-10-20 16:45:06 -0400

    split out compile options into default (O3) or -debug

--- compile.sh
+++ compile.sh
@@ -1,13 +1,20 @@
 #regular binary target
-if [[ $1 -eq 'binary' ]]
+if [[ $1 -eq 'binary-debug' ]]
 then
     g++ -g -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
     g++ -g -Wall -Wextra -Werror -c main.cpp -o main.o -std=c++11
     g++ main.o potential.o -o main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
 fi
 
+if [[ $1 -eq 'binary' ]]
+then
+    g++ -pipe -O3 -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
+    g++ -pipe -O3 -Wall -Wextra -Werror -c main.cpp -o main.o -std=c++11
+    g++ main.o potential.o -o main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
+fi
+
 #pybind target
-if [[ $1 == 'python' ]]
+if [[ $1 == 'python-debug' ]]
 then
     #compile potential.o
     g++ -g -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
@@ -15,5 +22,16 @@ then
     #whole lot of flag magic to get pybind11 to compile a .so for python
     includes=`python -m pybind11 --includes`
     g++ -Wall -shared -fPIC -std=c++11 $includes -undefined dynamic_lookup \
+        binding.cpp potential.o -o potential.so
+fi
+
+if [[ $1 == 'python' ]]
+then
+    #compile potential.o
+    g++ -pipe -O3 -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
+
+    #whole lot of flag magic to get pybind11 to compile a .so for python
+    includes=`python -m pybind11 --includes`
+    g++ -pipe -Wall -O3 -shared -fPIC -std=c++11 $includes -undefined dynamic_lookup \
         binding.cpp potential.o -o potential.so
 fi
