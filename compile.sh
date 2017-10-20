87a5e65de2f6d4a933fe0025d5664179e7df6930
Aditya Limaye <amlimaye@berkeley.edu>
2017-10-20 15:39:03 -0400

    split potential.hpp out into .cpp file

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
+g++ -g -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
 g++ -g -Wall -Wextra -Werror -c main.cpp -o main.o -std=c++11
-g++ main.o -o main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
+g++ main.o potential.o -o main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
--- potential.hpp
+++ potential.hpp
@@ -1,3 +1,5 @@
+#include "types.hpp"
+
 //ABC for potential classes
 class Potential {
 public:
@@ -7,23 +9,9 @@ public:
 
 class AsymmetricOneWell : Potential {
 public:	
-	AsymmetricOneWell(real_t a_x_curvature, real_t a_y_curvature) {
-		x_curvature = a_x_curvature;
-		y_curvature = a_y_curvature;
-	};
-
-	real_t energy (real_vec_t position) {
-		real_t x_sq = position[0]*position[0];
-		real_t y_sq = position[1]*position[1];
-		return (x_curvature*x_sq + y_curvature*y_sq);
-	};
-
-	real_vec_t gradient (real_vec_t position) {
-		real_t x_two = 2*position[0];
-		real_t y_two = 2*position[1];
-		real_vec_t grad = {x_curvature * x_two, y_curvature * y_two};
-		return grad;	
-	};
+	AsymmetricOneWell(real_t a_x_curvature, real_t a_y_curvature);
+	real_t energy (real_vec_t position); 
+	real_vec_t gradient (real_vec_t position);
 
 private:
 	real_t x_curvature;
@@ -32,22 +20,9 @@ private:
 
 class OneWell : Potential {
 public:	
-	OneWell(real_t a_curvature) {
-		curvature = a_curvature;
-	};
-
-	real_t energy (real_vec_t position) {
-		real_t x_sq = position[0]*position[0];
-		real_t y_sq = position[1]*position[1];
-		return curvature*(x_sq + y_sq);
-	};
-
-	real_vec_t gradient (real_vec_t position) {
-		real_t x_two = 2*position[0];
-		real_t y_two = 2*position[1];
-		real_vec_t grad = {x_two, y_two};
-		return curvature*grad;	
-	};
+	OneWell(real_t a_curvature);
+	real_t energy (real_vec_t position);
+	real_vec_t gradient (real_vec_t position);
 
 private:
 	real_t curvature;
@@ -55,30 +30,7 @@ private:
 
 class TwoWell : Potential {
 public:
-
 	TwoWell() {};
-
-	real_t energy (real_vec_t position) {
-		auto x = position[0];
-		auto y = position[1];
-
-		auto x2 = x*x;
-		auto x4 = x2*x2;
-		auto y2 = y*y;
-
-		auto energy = x4 - x2 + y2;
-		return energy;
-	};
-
-	real_vec_t gradient (real_vec_t position) {
-		auto x = position[0];
-		auto y = position[1];
-
-		auto x3 = x * x * x;
-
-		auto grad_x = 4 * x3 - 2*x;
-		auto grad_y = 2*y;
-		real_vec_t grad = {grad_x, grad_y};
-		return grad;
-	};
+	real_t energy (real_vec_t position);
+	real_vec_t gradient (real_vec_t position);
 };
