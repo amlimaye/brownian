7ef887950ee0b86fabd34ab3affe4c8263e888f6
Aditya Limaye <amlimaye@berkeley.edu>
2017-10-29 17:12:43 -0400

    changed directory structure

--- compile.sh
+++ compile.sh
@@ -1,37 +1,40 @@
+BUILD_DIR='.'
+SRC_DIR='src'
+
 #regular binary target
 if [[ $1 -eq 'binary-debug' ]]
 then
-    g++ -g -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
-    g++ -g -Wall -Wextra -Werror -c main.cpp -o main.o -std=c++11
-    g++ main.o potential.o -o main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
+    g++ -g -Wall -Wextra -Werror -c ${SRC_DIR}/potential.cpp -o ${BUILD_DIR}/potential.o -std=c++11
+    g++ -g -Wall -Wextra -Werror -c ${SRC_DIR}/main.cpp -o ${BUILD_DIR}/main.o -std=c++11
+    g++ ${BUILD_DIR}/main.o ${BUILD_DIR}/potential.o -o main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
 fi
 
 if [[ $1 -eq 'binary' ]]
 then
-    g++ -pipe -O3 -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
-    g++ -pipe -O3 -Wall -Wextra -Werror -c main.cpp -o main.o -std=c++11
-    g++ main.o potential.o -o main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
+    g++ -pipe -O3 -Wall -Wextra -Werror -c ${SRC_DIR}/potential.cpp -o ${BUILD_DIR}/potential.o -std=c++11
+    g++ -pipe -O3 -Wall -Wextra -Werror -c ${SRC_DIR}/main.cpp -o ${BUILD_DIR}/main.o -std=c++11
+    g++ ${BUILD_DIR}/main.o ${BUILD_DIR}/potential.o -o ${BUILD_DIR}/main -L/usr/local/Cellar/jsoncpp/1.8.3/lib/ -ljsoncpp
 fi
 
 #pybind target
 if [[ $1 == 'python-debug' ]]
 then
     #compile potential.o
-    g++ -g -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
+    g++ -g -Wall -Wextra -Werror -c ${SRC_DIR}/potential.cpp -o ${BUILD_DIR}/potential.o -std=c++11
 
     #whole lot of flag magic to get pybind11 to compile a .so for python
     includes=`python -m pybind11 --includes`
     g++ -Wall -shared -fPIC -std=c++11 $includes -undefined dynamic_lookup \
-        binding.cpp potential.o -o potential.so
+        ${SRC_DIR}/binding.cpp ${BUILD_DIR}/potential.o -o ${BUILD_DIR}/potential.so
 fi
 
 if [[ $1 == 'python' ]]
 then
     #compile potential.o
-    g++ -pipe -O3 -Wall -Wextra -Werror -c potential.cpp -o potential.o -std=c++11
+    g++ -pipe -O3 -Wall -Wextra -Werror -c ${SRC_DIR}/potential.cpp -o ${BUILD_DIR}/potential.o -std=c++11
 
     #whole lot of flag magic to get pybind11 to compile a .so for python
     includes=`python -m pybind11 --includes`
     g++ -pipe -Wall -O3 -shared -fPIC -std=c++11 $includes -undefined dynamic_lookup \
-        binding.cpp potential.o -o potential.so
+        ${SRC_DIR}/binding.cpp ${BUILD_DIR}/potential.o -o ${BUILD_DIR}/potential.so
 fi
